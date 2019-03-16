1ad7a746a1746abe183337163c70f202030c497e
yangying <yangying@souche.com>
2019-03-16 16:27:24 +0800

    解析源码HTML，返回源码中所有URL

--- Crawler/pachong1.py
+++ Crawler/pachong1.py
@@ -1,9 +1,27 @@
 import urllib.request
+from bs4 import BeautifulSoup   #调用bs4包的Be功能，解析源码
 
+
+#定义方法，返回URL源码！
 def hqyuanma(url):
-    re = urllib.request.urlopen(url,timeout=2)
+    re = urllib.request.urlopen(url, timeout=2)
     yuanma = re.read().decode()
-    print(yuanma)
+    # print(yuanma)
     return yuanma
 
-hqyuanma("https://souche.com")
+
+#定义方法，解析源码！
+def jiexi(yuanma):
+    code_obj = BeautifulSoup(yuanma, "html.parser")     #解析
+    # print(code_obj)
+    urls = code_obj.find_all('a')      #用find_all方法，根据共有标签a，找到源码中所有URL
+    print(urls)                        #打印list格式
+    for u in urls:
+        print(u)                       #打印格式好看~
+    return
+
+
+url = "https://baidu.com"              #定义变量data
+code = hqyuanma(url)                   #调用获取源码方法
+jiexi(code)                            #调用解析HTML，返回HTML中所有URL
+
