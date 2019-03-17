40bc919cc500f0a9d4767ad9db3878341b1f9ff5
yangying <yangying@souche.com>
2019-03-17 19:52:41 +0800

    提取url

--- Crawler/pachong1.py
+++ Crawler/pachong1.py
@@ -13,15 +13,20 @@ def hqyuanma(url):
 #定义方法，解析源码！
 def jiexi(yuanma):
     code_obj = BeautifulSoup(yuanma, "html.parser")     #解析
-    # print(code_obj)
-    urls = code_obj.find_all('a')      #用find_all方法，根据共有标签a，找到源码中所有URL
-    print(urls)                        #打印list格式
-    for u in urls:
-        print(u)                       #打印格式好看~
-    return
+    tags = code_obj.find_all('a')      #用find_all方法，根据共有标签a，找到源码中所有URL
+    urls = set()
+    for tag in tags:
+        print(tag)
+        url = tag['href']
+        print(url)
+        urls.add(url)
+    print(len(urls))
+    return urls
+
 
 
 url = "https://baidu.com"              #定义变量data
 code = hqyuanma(url)                   #调用获取源码方法
-jiexi(code)                            #调用解析HTML，返回HTML中所有URL
+a = jiexi(code)                            #调用解析HTML，返回HTML中所有URL
+print(a)
 
