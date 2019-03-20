ed959b714f09f4be34f6e6b2d525ef9c687e110b
yangying <yangying@souche.com>
2019-03-20 20:54:13 +0800

    jihe基础

---
+++ Crawler/jihe.py
@@ -0,0 +1,42 @@
+
+# set()是一个无序的不重复元素集合
+
+#创建集合
+jihe1 = set()
+jihe2 = {'A','123','[LIST,22]','apple','草莓'}
+print(jihe2)
+
+
+jihe2.add('A')  #1.添加元素
+print(jihe2)
+
+jihe2.update('[1]')     #添加元素
+print(jihe2)
+
+jihe2.remove('[')   #2.移除元素，元素不存在会报错
+print(jihe2)
+
+jihe2.discard(666)  #移除元素，元素不存在不会报错
+print(jihe2)
+
+jihe2.pop()     #随机删除一个元素
+x = jihe2.pop()
+print(x)
+print(jihe2)
+
+jihe3 = set(('1','2'))      #在交互模式，pop 是删除集合的第一个元素（排序后的集合的第一个元素）
+print(jihe3)
+jihe3.pop()
+print(jihe3)
+
+print(len(jihe3))       #3.计算集合元素个数
+
+jihe3.clear()       #4.清空集合元素
+print(jihe3)
+
+"A" in jihe2    #判断元素是否在集合中存在
+
+
+jihe4 = jihe2.copy()        #copy集合
+print(jihe4)
+
