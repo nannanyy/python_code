301cb5908568114dfae75e9db55a4e2f4ada0b34
yy <yy@yydeMacBook-Pro.local>
2019-04-07 15:33:01 +0800

    TC保存到文件

--- Crawler/test_server.py
+++ Crawler/test_server.py
@@ -4,7 +4,7 @@ from bs4 import BeautifulSoup
 
 
 def request_url(url):
-    re = urllib.request.urlopen(url, timeout=1)
+    re = urllib.request.urlopen(url)
     content = re.read().decode()
     return content
 
@@ -27,6 +27,8 @@ def request_server(op, param1, param2):
 def test(op, num):
     a = 0
     unpassed_expamles = set()
+    fo1 = open('tg_tc', 'a+')
+    fo2 = open('btg_tc','a+')
     for i in range(num):
         data = [random.random(), random.uniform(-100000000, 100000000), random.randint(-100000000, 100000000)]
         param1 = random.choice(data)
@@ -43,13 +45,21 @@ def test(op, num):
             print("op参数错误")
             return None
         server_res = request_server(op, param1, param2)
-        print('测试'+op+':', param1, param2, '期望：'+str(right_res), '验证:'+str(server_res))
+        data = '测试'+op+': '+str(param1) +', '+str(param2)+', 期望:'+str(right_res)+', 验证:'+str(server_res)+'\n'
+        print(data)
         if right_res == server_res:
             print("测试通过\n")
             a += 1
+            fo1.writelines(data)
+            fo1.readline()
         else:
             print("测试不通过\n")
             unpassed_expamles.add((op, param1, param2, right_res, server_res))
+            fo2.writelines(data)
+            fo2.readline()
+    fo1.writelines(str(a/num)+'\n')
+    fo1.close()
+    fo2.close()
     print("测试通过率", a/num)
     print("未通过样本", unpassed_expamles)
 
